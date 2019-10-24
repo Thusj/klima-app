1b1a4861ae9600c39ef22f00093119521cce6f47
Espen Thorsen Frank <Thusj@users.noreply.github.com>
2019-10-24 22:19:17 +0200

    some cleanup

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-class Titles extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Weather Finder</h1>
-                <p>Finn ut temperatur, forhold osv</p>
-            </div>
-        )
-    }
-}
+const Titles = () => (
+    <div>
+        <h1>Klima</h1>
+        <p>Finn ut temperatur, forhold osv</p>
+    </div>
+);
 
 export default Titles;
