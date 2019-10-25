7426e32ca2cf9a75924e64d255d77e1a4f1fbe21
Espen Thorsen Frank <Thusj@users.noreply.github.com>
2019-10-26 00:12:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`api.openweathermap.org/data/2.5/forecast/daily?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     if (city && country) {
       this.setState({
